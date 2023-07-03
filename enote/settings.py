16c97abda4a39f233cd8e63890b9c27361da3f2f
Anmolqq <basnetanmol2020@gmail.com>
2023-07-03 21:55:28 +0545

    Updated python runtime for vercel

--- enote/settings.py
+++ enote/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$-6@enjo86w75q$#req8&y5ggwxhz5o$zk5cxu4*(8epjzri0p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 
